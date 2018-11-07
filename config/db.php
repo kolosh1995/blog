09d4675cbafafc3e4bfd3966fd89fa29f9f60aac
kolosh <43340302+kolosh1995@users.noreply.github.com>
2018-11-07 21:53:45 +0200

    .env

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=blog',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=' . env('DB_HOST', '127.0.0.1') . ';dbname=' . env('DB_NAME', 'root'),
+    'username' => env('DB_USERNAME', 'root'),
+    'password' => env('DB_PASSWORD', 'root'),
     'charset' => 'utf8',
 ];
--- web/index.php
+++ web/index.php
@@ -1,13 +1,19 @@
 <?php
 
+use Dotenv\Dotenv;
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../helpers/helpers.php');
+
+(new Dotenv(__DIR__ . '/../'))->load();
+
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', env('YII_ENV'));
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require (__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require __DIR__ . '/../config/web.php';
+$config = require(__DIR__ . '/../config/web.php');
 require_once __DIR__ . '/../function.php';
 
 (new yii\web\Application($config))->run();
