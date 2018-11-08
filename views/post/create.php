954d2144120f7b1727cba2326dcccf5aa2232b48
kolosh <43340302+kolosh1995@users.noreply.github.com>
2018-11-08 23:29:23 +0200

    Update views/post/create.php

---
+++ views/post/create.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Category;
+use mihaildev\ckeditor\CKEditor;
+use mihaildev\elfinder\ElFinder;
+
+mihaildev\elfinder\Assets::noConflict($this);
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Post */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="post-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+
+    <?= $form->field($model, 'category_id')
+        ->dropDownList(ArrayHelper::map(Category::find()->all(), 'id', 'name')) ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?php
+    echo $form->field($model, 'description')->widget(CKEditor::className(), [
+        'editorOptions' => ElFinder::ckeditorOptions('elfinder', [])
+    ]);
+
+    ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
