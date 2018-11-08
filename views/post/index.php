a0ac09eaa315e7308a8b9dbe5af07290e86f3916
kolosh <43340302+kolosh1995@users.noreply.github.com>
2018-11-08 23:29:26 +0200

    Update views/post/index.php

---
+++ views/post/index.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Статьи';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="post-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Создать статью', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            [
+                'attribute' => 'author_id',
+                'value' => function ($model) {
+                    return $model->author->username;
+                },
+            ],
+
+            [
+                'attribute' => 'category_id',
+                'value' => function ($model) {
+                    return $model->category->name;
+                },
+            ],
+            'title',
+            'description:html',
+            'status',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
